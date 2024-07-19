525716fc2bae7582a9a758ad84ff9a8c1ac6993c
Andi Alfian <andi.putra@idntimes.com>
2024-07-19 17:27:50 +0700

    refactor: implement loading animation when fetching API

--- src/ui/home/HomeContainer.tsx
+++ src/ui/home/HomeContainer.tsx
@@ -5,28 +5,31 @@ import { getExchangeRates } from "@/modules/exchange/itemSevices";
 import { EXCHANGE_SAMPLE_RESPONSE } from "@/modules/exchange/samples";
 
 export default function HomeContainer(){
-    const [exchangeRates, setExchangeRates] = useState<ExchangeReponse>({"Realtime Currency Exchange Rate": EXCHANGE_SAMPLE_RESPONSE});
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [exchangeRates, setExchangeRates] = useState<ExchangeReponse>({"Realtime Currency Exchange Rate": null});
     const handleGetExchangeRates = async ({from_currency, to_currency}: PayloadExchangeRates) => {
         try {
-            // const res = await getExchangeRates({
-            //     from_currency,
-            //     to_currency
-            // })
-            // setExchangeRates(res);
+            setIsLoading(true)
+            const res = await getExchangeRates({
+                from_currency,
+                to_currency
+            })
+            setExchangeRates(res);
         } catch (e){
             console.log(e)
+        } finally {
+            setIsLoading(false);
         }
     }
 
     useEffect(() => {
         handleGetExchangeRates({
-            from_currency: 'JPY',
-            to_currency: 'USD'
+            from_currency: 'USD',
+            to_currency: 'JPY'
         })
     }, [])
 
-    console.log(exchangeRates);
     return (
-        <HomePage exchangeRates={exchangeRates['Realtime Currency Exchange Rate']} />
+        <HomePage isLoading={isLoading} exchangeRates={exchangeRates['Realtime Currency Exchange Rate']} />
     )
 }
--- src/ui/home/HomePage.tsx
+++ src/ui/home/HomePage.tsx
@@ -1,39 +1,49 @@
 import React from "react";
-import { Text } from '@/components';
-import clsx from 'clsx';
+import { Text } from "@/components";
+import clsx from "clsx";
 import { ExchangeRatesItems } from "@/modules/exchange/itemEntity";
 
-
 export default function HomePage(props: {
-  exchangeRates: ExchangeRatesItems | null
+  exchangeRates: ExchangeRatesItems | null;
+  isLoading: boolean;
 }) {
-  const { exchangeRates } = props
-  
+  const { exchangeRates, isLoading } = props;
+
   return (
-    <main 
-      className={clsx(
-        'relative',
-        'w-full',
-        'justify-center',
-        'p-10'
-      )}>
-      <Text textType="heading" className={clsx('text-3xl')}>Exchange Rates</Text>
-      <br/>
+    <main className={clsx("relative", "w-full", "justify-center", "p-10")}>
+      <Text textType="heading" className={clsx("text-3xl")}>
+        Exchange Rates
+      </Text>
+      <br />
       <section>
-        <div className="p-2 shadow-lg w-[300px]">
-          <div className={clsx('flex', 'flex-col')}>
-            <Text textType="subtitle" className={clsx('text-sm', 'text-gray-500')}>
-              1 {exchangeRates?.["2. From_Currency Name"]} equals
-            </Text>
-            <Text textType="subtitle" className={clsx('text-xl', 'text-black')}>
-            {exchangeRates?.["5. Exchange Rate"]} {exchangeRates?.["4. To_Currency Name"]}
-            </Text>
+        {isLoading ? (
+          <div className="max-w-sm mx-auto">
+            <div className={clsx('shimmer','h-40', 'mb-4', 'rounded')}></div>
           </div>
-          <div className="mt-10">
-            <Text textType="subtitle" className="text-xs text-gray-400">Last Refreshed {exchangeRates?.["6. Last Refreshed"]}</Text>
+        ) : (
+          <div className="p-2 shadow-lg w-[300px]">
+            <div className={clsx("flex", "flex-col")}>
+              <Text
+                textType="subtitle"
+                className={clsx("text-sm", "text-gray-500")}
+              >
+                1 {exchangeRates?.["2. From_Currency Name"]} equals
+              </Text>
+              <Text
+                textType="subtitle"
+                className={clsx("text-xl", "text-black")}
+              >
+                {exchangeRates?.["5. Exchange Rate"]}{" "}
+                {exchangeRates?.["4. To_Currency Name"]}
+              </Text>
+            </div>
+            <div className="mt-10">
+              <Text textType="subtitle" className="text-xs text-gray-400">
+                Last Refreshed {exchangeRates?.["6. Last Refreshed"]}
+              </Text>
+            </div>
           </div>
-        </div>
-     
+        )}
       </section>
       <section></section>
     </main>
